52fbd71ef03a12d8ccb18f7c528e2d3b01b37072
Werner Kroneman <werner@wernerkroneman.nl>
2025-07-22 12:10:34 +0200

    Add tests for DataFrame with categorical index and Vec data

--- src/data_frame.rs
+++ src/data_frame.rs
@@ -42,4 +42,39 @@ where
     fn index(&self, value: I::Value) -> &Self::Output {
         &self.data[self.index.to_flat_index(value)]
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::mapped_index::categorical_index::{CategoricalIndex, CategoricalValue};
+    use crate::mapped_index::MappedIndex;
+    use std::marker::PhantomData;
+
+    #[derive(Copy, Clone, Debug, PartialEq, Eq)]
+    struct Tag;
+
+    #[test]
+    fn test_dataframe_get_and_index() {
+        let index = CategoricalIndex { values: vec!["a", "b", "c"], _phantom: PhantomData::<Tag> };
+        let data = vec![10, 20, 30];
+        let df = DataFrame { index, data, _phantom: PhantomData };
+        let val = df.index.from_flat_index(1);
+        assert_eq!(df.get(val), &20);
+        assert_eq!(df[val], 20);
+        assert_eq!(df.get_flat(2), &30);
+    }
+
+    #[test]
+    fn test_dataframe_round_trip() {
+        let index = CategoricalIndex { values: vec!["x", "y", "z"], _phantom: PhantomData::<Tag> };
+        let data = vec![100, 200, 300];
+        let df = DataFrame { index, data, _phantom: PhantomData };
+        for flat in 0..df.index.size() {
+            let val = df.index.from_flat_index(flat);
+            let round = df.index.to_flat_index(val);
+            assert_eq!(flat, round);
+            assert_eq!(df.get(val), &df.get_flat(flat));
+        }
+    }
 } 
