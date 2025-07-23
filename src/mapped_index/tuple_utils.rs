b518f273265805fd1fbc51ebfaac8b7205f1f7d1
Werner Kroneman <werner@wernerkroneman.nl>
2025-07-23 13:45:59 +0200

    Added the ExtractAt extension trait for more ergonomic usage.

--- src/mapped_index/tuple_utils.rs
+++ src/mapped_index/tuple_utils.rs
@@ -220,7 +220,7 @@ pub trait TupleExtract<N: NonNeg> {
     type Result;
     type After;
 
-    fn extract(self) -> (Self::Before, Self::Result, Self::After);
+    fn extract_recursive(self) -> (Self::Before, Self::Result, Self::After);
 }
 
 impl<T: TupleFirstElement> TupleExtract<Zero> for T {
@@ -228,7 +228,7 @@ impl<T: TupleFirstElement> TupleExtract<Zero> for T {
     type Result = T::First;
     type After = T::Rest;
 
-    fn extract(self) -> (Self::Before, Self::Result, Self::After) {
+    fn extract_recursive(self) -> (Self::Before, Self::Result, Self::After) {
         self.split_first().prepend(())
     }
 }
@@ -245,16 +245,38 @@ where
     type Result = <<T as TupleFirstElement>::Rest as TupleExtract<N>>::Result;
     type After = <<T as TupleFirstElement>::Rest as TupleExtract<N>>::After;
 
-    fn extract(self) -> (Self::Before, Self::Result, Self::After) {
+    fn extract_recursive(self) -> (Self::Before, Self::Result, Self::After) {
         let (h, t) = self.split_first();
-        let (b, r, a) = t.extract();
+        let (b, r, a) = t.extract_recursive();
         (b.prepend(h), r, a)
     }
 }
 
-// Helper method to avoid having to write out the trait every time.
-pub fn extract<N: NonNeg, T: TupleExtract<N>>(tuple: T) -> (T::Before, T::Result, T::After) {
-    tuple.extract()
+pub trait ExtractAt {
+    fn extract_at<N: NonNeg>(
+        self,
+    ) -> (
+        <Self as TupleExtract<N>>::Before,
+        <Self as TupleExtract<N>>::Result,
+        <Self as TupleExtract<N>>::After,
+    )
+    where
+        Self: TupleExtract<N>;
+}
+
+impl<T> ExtractAt for T {
+    fn extract_at<N: NonNeg>(
+        self,
+    ) -> (
+        <Self as TupleExtract<N>>::Before,
+        <Self as TupleExtract<N>>::Result,
+        <Self as TupleExtract<N>>::After,
+    )
+    where
+        Self: TupleExtract<N>,
+    {
+        self.extract_recursive()
+    }
 }
 
 #[cfg(test)]
@@ -331,11 +353,11 @@ mod tests {
             let tuple = (1, "a", 3.14, true, 'x');
 
             // Extract the second element (index 1)
-            let (_, extracted, _) = extract::<peano::P1, _>(tuple);
+            let (_, extracted, _) = tuple.extract_at::<peano::P1>();
             assert_eq!(extracted, "a");
 
             // Extract the fourth element (index 3)
-            let (_, extracted, _) = extract::<peano::P3, _>(tuple);
+            let (_, extracted, _) = tuple.extract_at::<peano::P3>();
             assert_eq!(extracted, true);
         }
     }
